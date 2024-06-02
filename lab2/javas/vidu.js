c7fcf638e0c6bf85f98e4ef12436f4fd4f4fa31d
chuong-lee <chuongldps37998@gmail.com>
2024-06-02 16:56:08 +0700

    Đây là dự án đầu tiên cua tôi

---
+++ lab2/javas/vidu.js
@@ -0,0 +1,22 @@
+//var a=["a","b","c","d"];
+//console.log(a);
+//a.push("e");
+//console.log(a);
+
+//a.splice(a.length,0,"e")
+//console.log(a);
+
+//a.splice(3,0,"e")
+//console.log(a);
+
+//a.splice(2,1,"g","h")
+//a.pop()
+//console.log(a);
+
+
+//var arr5 = [1, 2, 3];
+//var arr6 = [4, 5, 6];
+//var arr7 = arr5.concat(arr6);
+//var arr8 = arr6.concat(arr5);
+//console.log(arr7);
+//console.log(arr8);
